b49ef2307ddf0305d3aea9e2f16818ee8bfedd0b
Ben-Bingham <ben@benbingham.ca>
2025-03-04 15:55:36 -0700

    Added min to board size

--- src/Main.cpp
+++ src/Main.cpp
@@ -414,7 +414,7 @@ int main() {
 
             if (boardSizeChange) {
                 if (ImGui::Button("Update Board Size")) {
-                    newBoardSize = glm::clamp(newBoardSize, glm::ivec2{ 0, 0 }, viewPortSize);
+                    newBoardSize = glm::clamp(newBoardSize, glm::ivec2{ 1, 1 }, viewPortSize);
                     boardSize = newBoardSize;
 
                     Texture::Parameters textureParameters{ };
